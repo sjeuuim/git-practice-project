27a6fdaba596bce23a63326b88fd633bc4c6e76a
Kim Taeyoon <xodbs97@gmail.com>
2023-09-18 20:23:21 +0900

    create myself kty.java

--- src/Taeyoon.java
+++
@@ -1,9 +0,0 @@
-
-public class Taeyoon {
-
-	public static void main(String[] args) {
-		System.out.println("Slim shady");
-		
-	}
-
-}
---
+++ src/com/chobo/myself/MyselfKTY.java
@@ -0,0 +1,9 @@
+package com.chobo.myself;
+
+public class MyselfKTY {
+
+	public static void main(String[] args) {
+		System.out.println("Àú´Â ±èÅÂÀ±ÀÔ´Ï´Ù.");
+	}
+
+}
